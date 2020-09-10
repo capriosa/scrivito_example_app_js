fbd766937b61479fe027db57a37105c657ee26b1
Meinolf Droste <mdwp.post@gmail.com>
2020-09-10 16:10:10 +0200

    new widgets

--- src/Widgets/index.js
+++ src/Widgets/index.js
@@ -4,3 +4,6 @@ function importAll(r) {
 
 // import all js files under src/Widgets/
 importAll(require.context("./", true, /\.js$/));
+import "scrivito-highlight-code";
+import "scrivito-news-reader"
+import "scrivito-highlight-code/index.css";
