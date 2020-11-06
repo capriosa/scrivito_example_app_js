8c9b4f3af9223baa89a2733acf382a53778b1cc3
Meinolf Droste <mdwp.post@gmail.com>
2020-11-06 16:31:37 +0100

    undraw fix

--- src/Widgets/index.js
+++ src/Widgets/index.js
@@ -7,5 +7,6 @@ importAll(require.context("./", true, /\.js$/));
 import "scrivito-highlight-code";
 import "scrivito-news-reader"
 import "scrivito-eyecatcher"
+import "scrivito-undraw"
 import "scrivito-highlight-code/index.css"
-import "scrivito-highlight-code/index.css";
+import "scrivito-undraw/index.css";
