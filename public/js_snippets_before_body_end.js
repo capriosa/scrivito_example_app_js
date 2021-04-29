21ce2304ca969c1a37c96a7d8b8be6ac2e9eea66
Meinolf Droste <mdwp.post@gmail.com>
2021-04-29 16:13:27 +0200

    fix

--- public/js_snippets_before_body_end.js
+++ public/js_snippets_before_body_end.js
@@ -1,6 +1,2 @@
 // This file is for executing JavaScript code immediately before the body element is closed.
 // Here you can, for example, let third-party code render additional markup.
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8156767702993247"
-  crossorigin="anonymous">
-  
-</script>
